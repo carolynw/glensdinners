27965cfda0967432e443016ab36f56c2bb714628
carolynw <carolynwarburton@hotmail.com>
2019-11-02 16:43:02 +0000

    Initial add of all files for ASP.Net core and blog

---
+++ BlogSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace miniblog
+{
+    public class BlogSettings
+    {
+        public string Name { get; set; } = "glensdinners";
+        public string Description { get; set; } = "Dinner's I've enjoyed";
+        public string Owner { get; set; } = "Glen Warburton";
+        public int PostsPerPage { get; set; } = 2;
+        public int CommentsCloseAfterDays { get; set; } = 10;
+    }
+}
