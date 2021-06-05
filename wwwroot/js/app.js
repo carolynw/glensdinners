ea32f94b19ef57e55dc0400be96732ea9800cfae
carolynw <carolynwarburton@hotmail.com>
2021-06-05 08:41:39 +0100

    Changes

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -105,7 +105,7 @@ new Vue({
         'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
         {
           maxZoom: 18,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+          attribution: '&copy; <a href=http://www.openstreetmap.org/copyright>OpenStreetMap</a>, &copy; <a href=https://carto.com/attribution>CARTO</a>',
         }
       );
         
